2f7d162f2a441b9b7c84aab0307ac5f31b2eb2f6
NeuralDynamic <neural.dynamic01k@gmail.com>
2020-02-25 19:11:39 +0200

    Completed second lesson to ML

--- Kaggle/Intro to Machine Learning/Creating a model with MAE.py
+++ Kaggle/Intro to Machine Learning/Creating a model with MAE.py
@@ -25,7 +25,7 @@ mean_absolute_error(y, predicted_home_prices)
 
 # Let's split our data in two pieces: training data and validation data, for both features and target
 from sklearn.model_selection import train_test_split
-train_X, val_X, train_y, val_y= train_test_split(X, y, random_state=0)
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 
 # Define model
 melbourne_model = DecisionTreeRegressor()
